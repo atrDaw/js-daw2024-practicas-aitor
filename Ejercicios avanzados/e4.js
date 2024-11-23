d03027ee45af67afc59bf7b5b62cfb97d59dd39d
Aitor <aitor.daw23@gmail.com>
2024-11-23 20:38:53 +0100

    avanzados modulos

---
+++ Ejercicios avanzados/e4.js
@@ -0,0 +1,5 @@
+import * as calc from "./modulos/calculadora.js"
+console.log(calc.suma(6,2))
+console.log(calc.resta(2,2))
+console.log(calc.divide(2,2))
+console.log(calc.multiplica(2,2))
---
+++ Ejercicios avanzados/modulos/calculadora.js
@@ -0,0 +1,13 @@
+function suma(a, b) {
+  return a + b;
+}
+function resta(a, b) {
+  return a - b;
+}
+function multiplica(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+export { suma, resta, multiplica, divide };
